1217d286d874da441b7a53a1707af1fef48b72fe
goghul <goghul.gocool@gmail.com>
2019-02-08 18:38:18 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int l;
+    scanf("%d",&l);
+    if(l%4==0)
+    {
+        printf("Leap Year");
+    }
+    else
+    {
+        printf("NOT");
+    }
+    return 0;
+}
