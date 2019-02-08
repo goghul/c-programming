ee578f547505185d2f87d177fa23dd5b6028ecca
goghul <goghul.gocool@gmail.com>
2019-02-08 19:34:21 +0530

    Create powern.c

---
+++ powern.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n,mul,d=1,i;
+	scanf("%d%d",&mul,&n);
+   for(i=1;i<=n;i++)
+   {
+       d=d*mul;
+     
+       }
+       
+		
+	
+		printf("%d",d);
+	
+	return 0;
+}
+
